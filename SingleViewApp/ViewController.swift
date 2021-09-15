7852d4ebb69332234477c4c7629e72719e3b71e9
Hari Bista <harikrista@gmail.com>
2021-09-15 13:44:05 +0545

    Initial Commit

---
+++ SingleViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Hari Bista on 9/15/21.
+//  Copyright © 2021 Hari Bista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
